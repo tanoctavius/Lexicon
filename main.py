59cc1c8f08e3fccb2aa91ea9ea21f3331df4e272
Octavius Tan <tanoctavius@gmail.com>
2023-07-01 18:45:29 +0800

    Added several details

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cmu_graphics import *
 from PIL import Image
 import button
 import mainScreen
+import loseScreen
 
 def onAppStart(app):
     app.width = 1200
@@ -10,14 +11,27 @@ def onAppStart(app):
     #Initialising Typing Text | Main Page:
     app.currText = "None"
     app.currTextColour = "Grey"
-    app.mainPageTitle = button.Label("lexico", (100, 50), 20, 'impact', app.currTextColour, 'center', False)
+    app.mainPageTitle = button.Label("l e x i c o", (200, 100), 25, 'impact', rgb(181, 94, 73), 'center', False)
     app.mainPageTypingText = button.Label(f"{app.currText}", (app.width//2, app.height//2), 60, 'impact', app.currTextColour, 'center', False)
+
+    app.keyboardIcon = Image.open('images/keyboardIcon.png')
+    app.keyboardIcon = CMUImage(app.keyboardIcon)
+
+    #Restarts if typing is lost
+    reset(app)
+
+def reset(app):
+    app.currChoiceColour = "Gray"
+    app.crazyCapital = button.Label("CrazyCapital", (240 + 120//2, 175), 15, 'impact', app.currChoiceColour, 'center', False)
+    app.crazyNumber = button.Label("CrazyNumber", (240 + 120//2 + 120, 175), 15, 'impact', app.currChoiceColour, 'center', False)
+    app.crazySpaces = button.Label("CrazySpaces", (240 + 120//2 + 240, 175), 15, 'impact', app.currChoiceColour, 'center', False)
+    app.uppercase = button.Label("Uppercase", (240 + 120//2 + 360, 175), 15, 'impact', app.currChoiceColour, 'center', False)
+    app.lowercase = button.Label("Lowercase", (240 + 120//2 + 480, 175), 15, 'impact', app.currChoiceColour, 'center', False)
+    app.mixed = button.Label("Mixed", (240 + 120//2 + 600, 175), 15, 'impact', app.currChoiceColour, 'center', False)
 
 def redrawAll(app):
     mainScreen.drawMainScreen(app)
     
-
-
 #-------------------------------------------------------------------------------
 #Opening Images
 def openImage(fileName):
--- mainScreen.py
+++ mainScreen.py
@@ -2,4 +2,18 @@ from cmu_graphics import *
 import button
 
 def drawMainScreen(app):
+    #Initialise the background:
+    drawRect(0, 0, app.width, app.height, fill = rgb(70, 75, 80))
+    drawImage(app.keyboardIcon, 105, 95, width = 40, height = 20)
+    drawCircle(app.width//2 - 350, 175, 25, fill = rgb(65, 65, 70))
+    drawCircle(970, 175, 25, fill = rgb(65, 65, 70))
+    drawRect(app.width//2 - 360, 150, 720, 50, fill = rgb(65, 65, 70))
+    
+    #Initialising the options
+    button.Label.drawLabel(app.crazyCapital)
+    button.Label.drawLabel(app.crazyNumber)
     button.Label.drawLabel(app.mainPageTitle)
+    button.Label.drawLabel(app.crazySpaces)
+    button.Label.drawLabel(app.mixed)
+    button.Label.drawLabel(app.lowercase)
+    button.Label.drawLabel(app.uppercase)
