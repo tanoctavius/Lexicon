f6bd24736f83a341aee68807693eba7ddc70b287
Octavius Tan <tanoctavius@gmail.com>
2023-07-02 09:00:37 +0800

    Added basic icon on loseScreen

--- loseScreen.py
+++ loseScreen.py
@@ -1,2 +1,10 @@
 from cmu_graphics import *
 import button
+
+def drawLoseScreen(app):
+    #Initialise the background:
+    drawRect(0, 0, app.width, app.height, fill = rgb(70, 75, 80))
+    drawImage(app.keyboardIcon, 105, 95, width = 40, height = 20)
+    button.Label.drawLabel(app.mainPageTitle)
+    button.Label.drawLabel(app.mainPageMiniTitle)
+    
